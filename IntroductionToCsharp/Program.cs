aa63c254d6d6c5a135e05bd68745424ff2a7e20d
Sarthak <sarthakdhakal@gmail.com>
2021-07-25 20:24:32 +0545

    initial commit

---
+++ IntroductionToCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroductionToCsharp
+{
+   public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("HI");
+        }
+    }
+}
